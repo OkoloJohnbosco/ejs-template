e53ee7f2f9333d7b257c33810536d0c8db59e6ca
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2022-11-14 15:47:18 +0100

    created index.ejs file

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 const port = 3000;
 
-
 // Static files
-app.use(express.static("public"))
-app.use("/css", express.static(__dirname + 'public/css'))
-app.use("/js", express.static(__dirname + 'public/js'))
-app.use("/img", express.static(__dirname + 'public/img'))
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
 
+// set views
+app.set("views", "./views");
+app.set("view engine", ".ejs");
 
 app.get("", (req, res) => {
-    // res.sendFile(__dirname + "/views/index.html")
-    res.send("/views/index.html")
-})
+  // res.sendFile(__dirname + "/views/index.html")
+  // res.send("/views/index.html")
+  res.render("index", {
+    text: "this is ejs",
+    profiles: [
+      { name: "Johnboscc", gender: "m" },
+      { name: "Goodness", gender: "f" },
+      { name: "Eze", gender: "m" },
+    ],
+  });
+});
+
+app.get("/about", (req, res) => {
+  // res.sendFile(__dirname + "/views/index.html")
+  // res.send("/views/index.html")
+  console.log(req.params);
+  res.render("about", {
+    text: "this is ejs",
+    data: { userQuery: req.params.userQuery, results: ["Johnbosc", "kdkdk"] },
+  });
+});
 // Listen on port 3000
-app.listen(port, () => console.info("Listening on port ${port}"))
+app.listen(port, () => console.info("Listening on port ${port}"));
